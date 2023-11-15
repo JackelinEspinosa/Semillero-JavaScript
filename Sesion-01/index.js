19a35dd92eccf806a8105c9c0de4ae34c08b21fc
JackelinEspinosa <134608173+JackelinEspinosa@users.noreply.github.com>
2023-11-15 09:49:13 -0500

    Sesion-1

---
+++ Sesion-01/index.js
@@ -0,0 +1,58 @@
+var deporte = "Atletismo";
+
+// Declaración (var). Cuando comienzas una variable, debes de utilizar la palabra var
+// Nombre (etiqueta). Irá del lado izquierdo. Será cómo llamarás la variable más adelante.
+// Asignación (=). A diferencia de la aritmética donde se le conoce como "igual", en Javascript se le conoce como asignación, el cual asignará el valor de lo que el área de su lado derecho genere hacia el lado izquierdo (Nombre).
+// Valor (Tipo de dato). Puede ser un texto, un número, un conjunto de datos (objetos, arreglos).
+
+// Primitivos
+// String
+// Number
+// Boolean
+
+// Compuestos (Referencia)
+// Object
+// Array
+// Function
+
+// Especiales
+// Undefined
+// Null
+
+//String
+var a = 'Hello World!';	// Comillas sencillas
+var b = "Hello World!";	// Comillas dobles
+
+var a = "Let's learn JavaScript.";	// Comilla sencilla dentro de comillas dobles
+var b = 'He said "Hello" and left.';	// Comillas dobles dentro de comillas sencillas
+var c = 'We\'ll never stop learning.';	// Escapando comilla sencilla con backslash
+
+//Number
+var a = 12;	// Entero
+var b = 32.43;	// Decimal
+var c = 2.25e+6;	// Equivalente a 2.25x10^6 o 2250000
+var d = 2.25e-6;	// Equivalente a 2.25x10-6 o 0.00000225
+
+//Boolean
+var isUserActive = true;	// Sí, el usuario se encuentra activo
+var isUserAdmin = false;	// No, el usuario no es admin
+
+//Especiales
+
+//Undefined
+var a;
+var b = 'Hello World';
+
+//console.log(a);	// undefined
+//console.log(b);	// Hello World
+
+//Null
+
+var a = 'Hello World';
+//console.log(a);	// Hello World
+
+a = null
+//console.log(a);	// null
+
+
+//typeof
