caffe3cd4ba8e1cbeac46d7965c0349cbbd4c1d1
JackelinEspinosa <134608173+JackelinEspinosa@users.noreply.github.com>
2023-11-29 09:42:31 -0500

    PostworkBedu

--- PostworkBedu/index.js
+++ PostworkBedu/index.js
@@ -1,41 +1,33 @@
-// Definir un array para almacenar las tareas
+
 var tasks = [];
 
-// Función para agregar una nueva tarea
 function addTask() {
-  // Obtener el elemento de entrada de la tarea
+
   var taskInput = document.getElementById('taskInput');
-  // Obtener la descripción de la nueva tarea
   var newTaskDescription = taskInput.value;
-  // Verificar que la descripción no esté vacía antes de agregar la tarea
+
   if (newTaskDescription.trim() !== '') {
     tasks.push({
       description: newTaskDescription,
       isCompleted: false,
       date: new Date().toLocaleDateString()  
     });
-    // Renderizar la lista de tareas actualizada
     renderTasks();
-    // Limpiar el campo de entrada de tarea después de agregarla
+
     taskInput.value = ''; 
   }
 }
 
-// Función para cambiar el estado completado/no completado de una tarea
 function toggleTaskCompleted(index) {
   if (tasks[index]) {
     tasks[index].isCompleted = !tasks[index].isCompleted;
     renderTasks();
   }
 }
-
-// Función para eliminar una tarea
 function deleteTask(index) {
   tasks.splice(index, 1);
   renderTasks();
 }
-
-// Función para renderizar la lista de tareas en el documento HTML
 function renderTasks() {
   var taskList = document.getElementById('taskList');
   taskList.innerHTML = ''; 
